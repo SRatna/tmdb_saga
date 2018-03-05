c64205ee82b337a9daa593b19405c3028854e547
SRatna <adhikariratna52@gmail.com>
2018-03-05 22:21:00 +0545

    add scroll to top functionality during new route load

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 // import PrivateRoute from '../containers/PrivateRoute';
-import Home from '../components/Home';
-import About from '../components/About';
+import Home from './Home';
+import About from './About';
+import ScrollToTop from './ScrollToTop';
 import Login from '../containers/Login';
 import MovieDetails from '../containers/MovieDetails';
 import PersonDetails from '../containers/PersonDetails';
@@ -10,13 +11,16 @@ import PersonDetails from '../containers/PersonDetails';
 let App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/movie/:id" component={MovieDetails} />
-        <Route path="/person/:id" component={PersonDetails} />
-        <Route path="/login" component={Login} />
-      </Switch>
+        <ScrollToTop>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/movie/:id" component={MovieDetails} />
+                <Route path="/person/:id" component={PersonDetails} />
+                <Route path="/login" component={Login} />
+            </Switch>
+        </ScrollToTop>
+
     </Router>
   );
 };
