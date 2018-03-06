867a88d6727ddbeec6b4e9be19865181db24bc80
sratna <adhikariratna52@gmail.com>
2018-03-06 09:45:42 +0545

    fix indentation in scrolltotop component

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,19 +1,19 @@
 /**
- * Created by osho on 3/5/18.
+ * Created by sushanta on 3/5/18.
  */
 import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
 class ScrollToTop extends Component {
-    componentDidUpdate(prevProps) {
-        if (this.props.location !== prevProps.location) {
-            window.scrollTo(0, 0);
-        }
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0);
     }
+  }
 
-    render() {
-        return this.props.children;
-    }
+  render() {
+    return this.props.children;
+  }
 }
 
 export default withRouter(ScrollToTop);
