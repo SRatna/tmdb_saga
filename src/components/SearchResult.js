1ff5cb59d6236fda60d8fbb31b86f984969bd45b
sratna <adhikariratna52@gmail.com>
2018-03-07 10:27:01 +0545

    create new component for handling movie search

---
+++ src/components/SearchResult.js
@@ -0,0 +1,44 @@
+/**
+ * Created by sushanta on 2/2/18.
+ */
+import React from 'react';
+import SearchMovie from '../containers/SearchMovie';
+import ShowMovie from '../containers/ShowMovie';
+import Header from '../containers/Header';
+import { Container } from 'semantic-ui-react'
+import { searchMovieRequest } from '../actions';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+class SearchResult extends React.Component {
+  componentDidMount() {
+    const { onMovieRequest, match } = this.props;
+    let movieName = match.params.query;
+    if (movieName) {
+      onMovieRequest(movieName);
+    }
+  }
+  componentWillReceiveProps(props) {
+    const { onMovieRequest, match } = props;
+    let movieName = match.params.query;
+    if (movieName) {
+      onMovieRequest(movieName);
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Container text style={{ marginTop: '4em' }}>
+          <SearchMovie />
+          <ShowMovie />
+        </Container>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = {
+  onMovieRequest: searchMovieRequest
+};
+export default withRouter(connect(null, mapDispatchToProps)(SearchResult));
