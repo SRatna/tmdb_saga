5db23e0d363a8ce6aabb53417a1e8168ccd6111d
sratna <adhikariratna52@gmail.com>
2018-03-07 13:09:41 +0545

    Rename movie component to movies

--- src/containers/ShowMovie.js
+++ src/containers/ShowMovie.js
@@ -2,7 +2,7 @@
  * Created by sushanta on 2/1/18.
  */
 import { connect } from 'react-redux';
-import Movie from '../components/Movie';
+import Movie from '../components/Movies';
 import { withRouter } from 'react-router-dom';
 
 const mapStateToProps = state => state.searchMovie;
