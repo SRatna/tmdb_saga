1d3305fa410422b5d42fd28872fccc1c575ea13a
sratna <adhikariratna52@gmail.com>
2018-03-07 12:44:52 +0545

    Add new nowPlayingMovies reducer to index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,9 +6,10 @@ import searchMovie from './searchMovie';
 import login from './login';
 import movieDetails from './movieDetails';
 import personDetails from './personDetails';
+import nowPlayingMovies from './nowPlayingMovies';
 
 const appReducer = combineReducers({
-  searchMovie, login, movieDetails, personDetails
+  searchMovie, login, movieDetails, personDetails, nowPlayingMovies
 });
 
 export default appReducer;
