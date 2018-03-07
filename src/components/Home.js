873984936822189dc38e0e1c0573329277242f4e
sratna <adhikariratna52@gmail.com>
2018-03-07 13:08:52 +0545

    Dispatch fetchNowPlayingMovies action during component did mount

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,32 @@
 import React from 'react';
 import SearchMovie from '../containers/SearchMovie';
 import Header from '../containers/Header';
+import NowPlayingMovies from '../containers/NowPlayingMovies';
 import { Container } from 'semantic-ui-react'
+import { connect } from 'react-redux';
+import { fetchNowPlayingMovies } from '../actions';
+import './Home.css';
 
-let Home = () => {
-  return (
-    <div>
-      <Header />
-      <Container text style={{ marginTop: '4em' }}>
-        <SearchMovie />
-      </Container>
-    </div>
-  );
+class Home extends React.Component {
+  componentDidMount() {
+    this.props.fetchNowPlayingMovies();
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Container text style={{ marginTop: '4em' }}>
+          <SearchMovie />
+          <span className="now-playing-title">Now Playing Movies</span>
+          <NowPlayingMovies />
+        </Container>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = {
+  fetchNowPlayingMovies
 };
 
-export default Home;
+export default connect(null, mapDispatchToProps)(Home);
